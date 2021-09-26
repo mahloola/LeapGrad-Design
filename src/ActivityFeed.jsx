30a7925e19fe95affa7203a785fee79efb245475
mahloola <mahloola08@gmail.com>
2021-09-25 22:00:20 -0400

    removed unnecessary code

--- src/ActivityFeed.jsx
+++ src/ActivityFeed.jsx
@@ -1,11 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ActivityDetail from './ActivityDetail.jsx'
 
-// async function getCall(id) {
-//   const res = await fetch(`https://aircall-job.herokuapp.com/activities/${id}`)
-//   return await res.json()
-// }
-
 const ActivityFeed = () => {
 
     const [activitiesPage, setActivitiesPage] = useState(null);
@@ -18,23 +13,12 @@ const ActivityFeed = () => {
             })
             .catch(err => console.error('Unable to load activity data: ', err));
     }, []);
-    // <Container className="activity">
-    //     <Row>
-    //         {
-    //             activities.map((activity) => (
-    //                 <ActivityDetail key={activity.id} activity={activity}></ActivityDetail>
-    //             ))
-    //         }
-    //         <Col>
-    //         </Col>
-    //     </Row>
-    // </Container>
 
     return (
         <div>
             {
               activitiesPage !== null ? activitiesPage.map((activity) => (
-                <ActivityDetail activity={activity} />
+                <ActivityDetail key={activity.id} activity={activity} />
                 )) : null
             }    
             
