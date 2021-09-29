0758e56a0285a3df48f778c9f76258a6bf873a03
Mahmoud <mahloola08@gmail.com>
2021-09-29 18:55:20 -0400

    added phone icons, duration, and more styling

--- src/ActivityDetail.jsx
+++ src/ActivityDetail.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import { BiPhoneIncoming, BiPhoneOutgoing } from "react-icons/bi"
 import './css/activitydetail.css'
 
 const ActivityDetail = ({ activity }) => {
     return (
         <div>
+
             <div className="activity-container">
-                {activity.from} called {activity.to}
-                
+
+                {activity.direction == "outbound" &&
+                    <div className="activity-phone-outbound">
+                        <BiPhoneOutgoing size={20}></BiPhoneOutgoing>
+                    </div>}
+
+                {activity.direction == "inbound" &&
+                    <div className="activity-phone-inbound">
+                        <BiPhoneIncoming size={20}></BiPhoneIncoming>
+                    </div>}
+
+                {activity.from}
+                <br />
+                <div className="activity-to">
+                    called {activity.to}
+                </div>
+                <div className="activity-duration">
+                    {activity.duration}
+                </div>
             </div>
         </div>
     )
